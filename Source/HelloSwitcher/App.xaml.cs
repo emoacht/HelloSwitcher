d9c65bb595eaef6b7ede6846b030324351894d73
emoacht <emotom@pobox.com>
2021-02-19 11:17:08 +0900

    Separate logger

--- Source/HelloSwitcher/App.xaml.cs
+++ Source/HelloSwitcher/App.xaml.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.IO;
 using System.Linq;
-using System.Media;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -24,9 +22,9 @@ namespace HelloSwitcher
 		{
 			base.OnStartup(e);
 
-			DispatcherUnhandledException += (_, e) => Record(e.Exception);
-			TaskScheduler.UnobservedTaskException += (_, e) => Record(e.Exception);
-			AppDomain.CurrentDomain.UnhandledException += (_, e) => Record(e.ExceptionObject);
+			DispatcherUnhandledException += (_, e) => Logger.RecordException(e.Exception);
+			TaskScheduler.UnobservedTaskException += (_, e) => Logger.RecordException(e.Exception);
+			AppDomain.CurrentDomain.UnhandledException += (_, e) => Logger.RecordException(e.ExceptionObject);
 
 			_settings = new Settings();
 			await _settings.LoadAsync();
@@ -111,19 +109,6 @@ namespace HelloSwitcher
 				return Task.CompletedTask;
 
 			return window.SearchAsync();
-		}
-
-		private void Record(object exception)
-		{
-			SystemSounds.Exclamation.Play();
-
-			var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), nameof(HelloSwitcher));
-			if (!Directory.Exists(folderPath))
-				Directory.CreateDirectory(folderPath);
-
-			var filePath = Path.Combine(folderPath, "exception.txt");
-
-			File.WriteAllText(filePath, $"[{DateTime.Now}]\r\n{exception}");
 		}
 
 		protected override void OnExit(ExitEventArgs e)
---
+++ Source/HelloSwitcher/Models/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSwitcher.Models
+{
+	internal static class Logger
+	{
+		private static string FolderPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), nameof(HelloSwitcher));
+		private static string OperationFilePath => Path.Combine(FolderPath, "operation.txt");
+		private static string ExceptionFilePath => Path.Combine(FolderPath, "exception.txt");
+
+		private static void EnsureFolder()
+		{
+			if (!Directory.Exists(FolderPath))
+				Directory.CreateDirectory(FolderPath);
+		}
+
+		public static void RecordOperation(string content, bool append = true)
+		{
+			EnsureFolder();
+
+			using var writer = new StreamWriter(OperationFilePath, append, Encoding.UTF8);
+			writer.Write($"[{DateTime.Now}]{Environment.NewLine}{content}{Environment.NewLine}{Environment.NewLine}");
+		}
+
+		public static void RecordException(object exception)
+		{
+			EnsureFolder();
+
+			File.WriteAllText(ExceptionFilePath, $"[{DateTime.Now}]{Environment.NewLine}{exception}");
+		}
+	}
+}
